7a6d706297e370d65babee8ebcedef0dbab0d0ff
sczahra <sczahra@gmail.com>
2026-01-31 05:54:55 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install',e=>{
- e.waitUntil(caches.open('gb-v3').then(c=>c.addAll(['./'])))
+ e.waitUntil(caches.open('gb-v4').then(c=>c.addAll(['./'])))
 });
 self.addEventListener('fetch',e=>{
  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
